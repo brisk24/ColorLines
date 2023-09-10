397a22aab79e14c9dde91ed91d7d918e711efdd8
brisk24 <krivick.aleksej@yandex.ru>
2023-09-10 21:15:46 +0300

    Исправление ошибки вывода шарика

--- ColorLines/Game.cs
+++ ColorLines/Game.cs
@@ -38,6 +38,7 @@ namespace ColorLines
 
 
         Ball marked_ball;
+        Ball destin_ball;
         int marked_jump;
         public void ClickBox(int x, int y)
         {
@@ -57,6 +58,17 @@ namespace ColorLines
                     return;
                 }
             }
+
+            if(status == Status.ball_mark)
+                if (map[x, y] <= 0)
+                {
+                    destin_ball.x = x;
+                    destin_ball.y = y;
+                    destin_ball.color = marked_ball.color;
+                    status = Status.ball_move;
+                    return;
+                }
+
         }
 
         public void Step()
@@ -81,6 +93,7 @@ namespace ColorLines
                     break;
                 case Status.next_balls:
                     ShowNextBalls();
+                    SelectNextBalls();                 
                     break;
                 case Status.line_strip:
                     StripLine();
@@ -97,7 +110,16 @@ namespace ColorLines
 
         private void MoveBall()
         {
-            throw new NotImplementedException();
+            if (status != Status.ball_move) return;
+            if (map[marked_ball.x, marked_ball.y] > 0 &&
+                map[destin_ball.x, destin_ball.y] <= 0)
+            {
+                map[marked_ball.x, marked_ball.y] = 0;
+                map[destin_ball.x, destin_ball.y] = marked_ball.color;
+                Show(marked_ball, Item.none);
+                Show(destin_ball, Item.ball);
+                status = Status.next_balls;
+            }
         }
 
         private void JumpBall()
@@ -115,13 +137,14 @@ namespace ColorLines
             ShowNextBall(ball[0]);
             ShowNextBall(ball[1]);
             ShowNextBall(ball[2]);
+            status = Status.wait;
         }
         private void ShowNextBall(Ball next)
         {
             if (next.x < 0) return;
             if (map[next.x, next.y] > 0)
             {
-                next = SelectNextBall();
+                next = SelectNextBall(next.color);
                 if (next.x < 0) return;
             }
             map[next.x, next.y] = next.color;
@@ -132,14 +155,18 @@ namespace ColorLines
         {
             ball[0] = SelectNextBall();
             ball[1] = SelectNextBall();
-            ball[2] = SelectNextBall();
+            ball[2] = SelectNextBall();          
         }
 
         Ball SelectNextBall()
         {
+            return SelectNextBall(random.Next(1, max_colors + 1));
+        }
+        Ball SelectNextBall(int color)
+        {
             int loop = 100;
             Ball next;
-            next.color = random.Next(1, max_colors + 1);
+            next.color = color;
             do
             {
                 next.x = random.Next(0, max);
